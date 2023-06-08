d2591003ed7e0e374aa71f4a35eb4c470918e979
Misha Tkachenko <misha.tkachenko@telink-semi.com>
2023-06-08 21:54:12 +0300

    b91_sleep: draft PM solution for B92 mcu
    draft PM solution for B92 MCU
    
    Signed-off-by: Misha Tkachenko <misha.tkachenko@telink-semi.com>

--- tlsr9/common/b91_sleep.h
+++ tlsr9/common/b91_sleep.h
@@ -30,7 +30,9 @@
 #ifndef __B91_SLEEP_H
 #define __B91_SLEEP_H
 
+bool b9x_suspend(uint32_t wake_stimer_tick);
 bool b91_suspend(uint32_t wake_stimer_tick);
+bool b92_suspend(uint32_t wake_stimer_tick);
 #ifdef CONFIG_BOARD_TLSR9518ADK80D_RETENTION
 bool b91_deep_sleep(uint32_t wake_stimer_tick);
 #endif /* CONFIG_BOARD_TLSR9518ADK80D_RETENTION */
