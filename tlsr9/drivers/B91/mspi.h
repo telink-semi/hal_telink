0e9de54ef200ed88607be9238e0251b2977b5bdd
Dima Horbenko <dima.horbenko@telink-semi.com>
2023-07-04 12:21:13 +0300

    hal: telink: flash: spi: Update flash driver with 4 wire spi read and write functions
    Use b91m flash driver files
    
    Signed-off-by: Dima Horbenko <dima.horbenko@telink-semi.com>

--- tlsr9/drivers/B91/mspi.h
+++ tlsr9/drivers/B91/mspi.h
@@ -1,27 +1,24 @@
-/******************************************************************************
- * Copyright (c) 2022 Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
- * All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- *****************************************************************************/
-
 /********************************************************************************************************
- * @file	mspi.h
+ * @file    mspi.h
  *
- * @brief	This is the header file for B91
+ * @brief   This is the header file for B91
  *
- * @author	Driver Group
+ * @author  Driver Group
+ * @date    2019
+ *
+ * @par     Copyright (c) 2019, Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ *
+ *          Licensed under the Apache License, Version 2.0 (the "License");
+ *          you may not use this file except in compliance with the License.
+ *          You may obtain a copy of the License at
+ *
+ *              http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *          Unless required by applicable law or agreed to in writing, software
+ *          distributed under the License is distributed on an "AS IS" BASIS,
+ *          WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *          See the License for the specific language governing permissions and
+ *          limitations under the License.
  *
  *******************************************************************************************************/
 #pragma once
@@ -29,6 +26,17 @@
 #include "compiler.h"
 #include "gpio.h"
 #include "reg_include/mspi_reg.h"
+
+
+/**
+ * @brief 	Data line mode of mspi
+ */
+typedef enum{
+	MSPI_SINGLE_LINE	= 0x00,
+	MSPI_DUAL_LINE		= 0x04,
+	MSPI_QUAD_LINE		= 0x0c,
+}mspi_data_line_e;
+
 
 /**
   * @brief     This function servers to set the spi wait.
@@ -39,19 +47,44 @@ _attribute_ram_code_sec_ static inline void mspi_wait(void){
 }
 
 /**
- * @brief     This function servers to enable read triggle spi.
+ * @brief     This function servers to enable read trigger spi.
  * @return    none.
  */
-_attribute_ram_code_sec_ static inline void mspi_fm_rd_en(void){
+_attribute_ram_code_sec_ static inline void mspi_fm_rd_trig_en(void){
 	reg_mspi_fm |= FLD_MSPI_RD_TRIG_EN;
 }
 
 /**
- * @brief     This function servers to disable read triggle spi.
+ * @brief     This function servers to disable read trigger spi.
  * @return    none.
  */
-_attribute_ram_code_sec_ static inline void mspi_fm_rd_dis(void){
+_attribute_ram_code_sec_ static inline void mspi_fm_rd_trig_dis(void){
 	reg_mspi_fm &= ~FLD_MSPI_RD_TRIG_EN;
+}
+
+/**
+ * @brief     This function is used to configure mspi to read mode.
+ * @return    none.
+ */
+_attribute_ram_code_sec_ static inline void mspi_fm_read_en(void){
+	reg_mspi_fm |= FLD_MSPI_RD_MODE;
+}
+
+/**
+ * @brief     This function is used to configure mspi to write mode.
+ * @return    none.
+ */
+_attribute_ram_code_sec_ static inline void mspi_fm_write_en(void){
+	reg_mspi_fm &= ~FLD_MSPI_RD_MODE;
+}
+
+/**
+ * @brief     This function is used to configure mspi for several-line communication mode.
+ * @param[in] line	- several-line communication mode.
+ * @return    none.
+ */
+_attribute_ram_code_sec_ static inline void mspi_fm_data_line(mspi_data_line_e line){
+	reg_mspi_fm = (reg_mspi_fm & (~FLD_MSPI_DATA_LINE)) | line;
 }
 
 /**
