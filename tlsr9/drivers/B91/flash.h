52ffa24b49666eab2725f6f4e306f0cae7093170
Dima Horbenko <dima.horbenko@telink-semi.com>
2023-07-05 12:54:25 +0300

    hal: telink: flash: Fix copyrights
    Fix copyrights
    
    Signed-off-by: Dima Horbenko <dima.horbenko@telink-semi.com>

--- tlsr9/drivers/B91/flash.h
+++ tlsr9/drivers/B91/flash.h
@@ -1,44 +1,38 @@
+/******************************************************************************
+ * Copyright (c) 2023 Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
+
 /********************************************************************************************************
- * @file    flash.h
+ * @file	flash.h
  *
- * @brief   This is the header file for B91
+ * @brief	This is the header file for B91
  *
- * @author  Driver Group
- * @date    2019
- *
- * @par     Copyright (c) 2019, Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
- *
- *          Licensed under the Apache License, Version 2.0 (the "License");
- *          you may not use this file except in compliance with the License.
- *          You may obtain a copy of the License at
- *
- *              http://www.apache.org/licenses/LICENSE-2.0
- *
- *          Unless required by applicable law or agreed to in writing, software
- *          distributed under the License is distributed on an "AS IS" BASIS,
- *          WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *          See the License for the specific language governing permissions and
- *          limitations under the License.
+ * @author	Driver Group
  *
  *******************************************************************************************************/
-/**	@page FLASH
- *
- *	Introduction
- *	===============
- *	supports basic flash functions
- *
- *	API Reference
- *	===============
- *	Header File: flash.h
- *
- *	Attention
+
+/*	Attention
  *	==============
  *  -# the description of parameters addr contained on the interface:
  *      - It is not necessary to add the base address 0x20000000, which ranges from 0 to the size of the flash storage space.
  *  -# by default,the flash functions that call sub-functions(flash_mspi_read_ram/flash_mspi_write_ram) cannot be interrupted by interrupts,
  *     if the function needs to be interrupted by a high priority interrupt during execution refer to flash_plic_preempt_config().
- * 
- */
+*/
+
 #pragma once
 
 #include "compiler.h"
