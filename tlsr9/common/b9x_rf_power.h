0e34f7bf18e07e3db228e9bfbabf9d27b593d229
Misha Tkachenko <misha.tkachenko@telink-semi.com>
2023-09-21 22:36:24 +0300

    telink: hal: drivers: Updated BLE RF Power config
    Applied config in same way as for 802154 network
    
    Signed-off-by: Misha Tkachenko <misha.tkachenko@telink-semi.com>

---
+++ tlsr9/common/b9x_rf_power.h
@@ -0,0 +1,29 @@
+/******************************************************************************
+ * Copyright (c) 2023 Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
+
+#ifndef B9X_BT_RF_POWER_H_
+#define B9X_BT_RF_POWER_H_
+
+#include "stdint.h"
+
+#define B9X_TX_POWER_MIN                    (-30)
+#define B9X_TX_POWER_MAX                    (9)
+
+extern const uint8_t b9x_tx_pwr_lt[];
+
+#endif /* B9X_BT_RF_POWER_H_ */
