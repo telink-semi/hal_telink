576d410662f1d533879ff8058a40f32d9204ed5e
pulin1103 <zhenghuan.zhang1103@gmail.com>
2024-08-29 14:06:23 +0800

    hal: telink: drivers: usb: update tl321x usb driver
    - fixed tl321x endpoint switch.
    - add driver_tl321x.h file.
    
    Signed-off-by: pulin1103 <zhenghuan.zhang1103@gmail.com>

---
+++ tlsr9/drivers/TL321X/driver_tl321x.h
@@ -0,0 +1,60 @@
+/******************************************************************************
+ * Copyright (c) 2024 Telink Semiconductor (Shanghai) Co., Ltd. ("TELINK")
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *****************************************************************************/
+
+/********************************************************************************************************
+ * @file	driver_tl321x.h
+ *
+ * @brief	This is the header file for TL321X
+ *
+ * @author	Driver Group
+ *
+ *******************************************************************************************************/
+#pragma once
+
+#include "adc.h"
+#include "aes.h"
+#include "analog.h"
+#include "audio.h"
+#include "clock.h"
+#include "core.h"
+#include "dma.h"
+//#include "eccp_curve.h"
+// #include "emi.h"
+#include "lib/include/flash_base.h"
+#include "flash.h"
+#include "gpio.h"
+#include "i2c.h"
+#include "lpc.h"
+//#include "mdec.h"
+#include "mspi.h"
+//#include "pke_utility.h"
+#include "lib/include/pke/pke.h"
+#include "lib/include/plic.h"
+#include "lib/include/pm.h"
+#include "pwm.h"
+#include "qdec.h"
+#include "lib/include/rf/rf_common.h"
+#include "s7816.h"
+#include "spi.h"
+#include "stimer.h"
+#include "lib/include/sys.h"
+#include "timer.h"
+#include "lib/include/trng/trng.h"
+#include "uart.h"
+#include "usbhw.h"
+#include "watchdog.h"
