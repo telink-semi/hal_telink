741229c3d14c3b9a50fc20801dba5768ff3e5bbb
Damien Ji <yinghao.ji@telink-semi.com>
2026-06-29 13:23:10 +0800

    hal: Update flash support for TL323X and TL721X
        - Add TL721X (TL7218X) 1MB/2MB flash support
        - Improve TL323X (TL3238X) flash protection for 1/2/4MB
        - Update SDK target to 46322e5
    
    Signed-off-by: Damien Ji <yinghao.ji@telink-semi.com>

--- hal_v2/wrapper/common/tl_flash.c
+++ hal_v2/wrapper/common/tl_flash.c
@@ -38,9 +38,24 @@
         #if (CONFIG_FLASH_SIZE > 2048)
         /* 4MB flash or larger, protect up to 3968K */
         #define FLASH_ADR_OFFSET_SELECT         FLASH_3968K_ADR_OFFSET
-        #else
-        /* 2MB flash or smaller, protect up to 1920K */
+        #elif (CONFIG_FLASH_SIZE > 1024)
+        /* 2MB flash, protect up to 1920K */
         #define FLASH_ADR_OFFSET_SELECT         FLASH_1920K_ADR_OFFSET
+        #else
+        /* 1MB flash, protect up to 1M */
+        #define FLASH_ADR_OFFSET_SELECT         FLASH_1M_ADR_OFFSET
+        #endif
+        #endif/*CONFIG_PM*/
+#elif CONFIG_SOC_RISCV_TELINK_TL721X
+        #if CONFIG_PM
+        #define FLASH_ADR_OFFSET_SELECT         FLASH_1920K_ADR_OFFSET
+        #else 
+        #if (CONFIG_FLASH_SIZE > 1024)
+        /* 2MB flash, protect up to 1920K */
+        #define FLASH_ADR_OFFSET_SELECT         FLASH_1920K_ADR_OFFSET
+        #else
+        /* 1MB flash, protect up to 1M */
+        #define FLASH_ADR_OFFSET_SELECT         FLASH_1M_ADR_OFFSET
         #endif
         #endif/*CONFIG_PM*/
 #else
